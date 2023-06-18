7d0e6fb741d1a0e96534f00bab14cdb9754ee079
moses <moses.kamau@student.moringaschool.com>
2023-06-18 23:28:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blockNo){
+    if(blockNo > 42){
+        return blockNo - 42;
+    }
+    else{
+        return 42 - blockNo;
+    }
+}
+
+function distanceFromHqInFeet(blockNo){
+   let feet = distanceFromHqInBlocks(34);
+    return feet * 264;
+}
+
+function distanceTravelledInFeet(start,destination){
+    if(destination > start){
+        return (destination - start) * 264;
+    }
+    else{
+        return (start - destination) * 264;
+    }
+}
+function calculatesFarePrice(start, destination){
+   let distance = distanceTravelledInFeet(start,destination);
+   if (distance <= 400){
+    return 0;
+   }
+   else if(distance> 400 && distance <= 2000){
+    return 0.02 * (distance - 400);
+   }
+   else if(distance > 2000 && distance < 2500){
+    return 25;
+   }
+   else
+   {
+    return 'cannot travel that far';
+   }
+}
